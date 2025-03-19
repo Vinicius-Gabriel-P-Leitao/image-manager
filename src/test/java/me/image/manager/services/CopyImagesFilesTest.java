00c3c21b2f1f9bf899dfc162b34bed7e5fed76ab
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2025-03-18 23:20:03 -0300

    [test]: Inicia a criação de testes no projeto
    TEST: Primeiro teste realizado, visando o service CopyImagesFiles que
    recebe como parametro origem, destino e uma progressbar, Foi realizado
    testes para os erros em potencial, erros como caminho de pasta invalido
    ou nulos e um teste de sucesso com uso para quando a movimentação de
    arquivos acontecer com sucesso.

---
+++ src/test/java/me/image/manager/services/CopyImagesFilesTest.java
@@ -0,0 +1,102 @@
+package me.image.manager.services;
+
+import javafx.application.Platform;
+import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CopyImagesFilesTest extends ApplicationTest {
+    private CopyImagesFiles copyImagesFiles;
+
+    @Override
+    public void start(Stage stage) {
+        copyImagesFiles = new CopyImagesFiles();
+    }
+
+    /**
+     * @throws IOException
+     */
+    @BeforeEach
+    void setUp() throws IOException {
+        Path directory = Paths.get("src/test/resources/destination");
+
+        if (Files.exists(directory)) {
+            try (Stream<Path> files = Files.walk(directory)) {
+                files.sorted(Comparator.reverseOrder()).forEach(path -> {
+                    try {
+                        Files.deleteIfExists(path);
+                    } catch (IOException e) {
+                        throw new RuntimeException("Erro ao excluir: " + path, e);
+                    }
+                });
+            }
+
+            // Garante que o próprio diretório seja deletado
+            Files.deleteIfExists(directory);
+        }
+    }
+
+    /**
+     * @throws IOException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void testCopyFilesWithValidPaths() throws IOException, ExecutionException, InterruptedException {
+        Path originPath = Paths.get("src/test/resources/origin/image.png");
+        Path destinationPath = Paths.get("src/test/resources/destination");
+        ProgressBar progressBar = new ProgressBar();
+
+        Path destinationFilePath = destinationPath.resolve("image.png");
+        copyImagesFiles.copyFiles(originPath, destinationFilePath, progressBar);
+
+
+        assertTrue(Files.exists(destinationFilePath), "O arquivo copiado não foi encontrado.");
+        assertTrue(Files.exists(destinationPath), "A pasta de destino não foi encontrada.");
+
+        Platform.runLater(() -> progressBar.setProgress(0.5));
+        progressBar.getProgress();
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testCopyFilesWithNullPaths() {
+        ProgressBar progressBar = new ProgressBar();
+
+        try {
+            copyImagesFiles.copyFiles(null, null, progressBar);
+        } catch (RuntimeException exception) {
+            assert exception.getMessage().contains("Os diretórios não podem ser vazios.");
+        }
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testAlertOnFileCopyError() {
+        Path originPath = Paths.get("invalid/path/to/");
+        Path destinationPath = Paths.get("destination/path");
+        ProgressBar progressBar = new ProgressBar();
+
+        try {
+            copyImagesFiles.copyFiles(originPath, destinationPath, progressBar);
+        } catch (RuntimeException exception) {
+            assert exception.getMessage().contains("Erro ao listar arquivos");
+        }
+    }
+}

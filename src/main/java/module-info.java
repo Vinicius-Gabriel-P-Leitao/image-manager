6960ad307c2fe6030bbff544e42a43ed2fa38442
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2025-04-08 04:06:23 -0300

    [feat]: Implementa dependencias para nova funcionalidade.
    Implementa as libs necessárias para abrir cliente OAUTH e salvar token e
    para usar o token do google drive.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,14 @@ module me.image.manager {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.zondicons;
     requires java.desktop;
+    requires com.google.api.client.json.gson;
+    requires com.google.api.client;
+    requires com.google.api.services.drive;
+    requires com.google.api.client.auth;
+    requires google.api.client;
+    requires com.google.api.client.extensions.java6.auth;
+    requires com.google.api.client.extensions.jetty.auth;
+    requires opencensus.api;
 
     opens me.image.manager to javafx.fxml;
     exports me.image.manager to javafx.graphics;
